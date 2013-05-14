b898031c25620ed360aa85ebfd1870e945134564
ArmNo <arm@snapplabs.com>
2013-05-14 21:49:46 +0700

    erb

---
+++ app.rb
@@ -0,0 +1,48 @@
+require "sinatra/base"
+
+IMAGES = [
+	{
+		title: "Photo 1",
+		url: "http://farm8.staticflickr.com/7287/8738336572_3588e1da15.jpg"
+	},
+	{
+		title: "Photo 2",
+		url: "http://farm8.staticflickr.com/7286/8736338425_a703305bbd.jpg"
+	},
+	{
+		title: "Photo 3",
+		url: "http://farm8.staticflickr.com/7330/8731315304_def198b68b.jpg" 
+	}
+]
+class App < Sinatra::Base
+
+	get "/images" do
+		@images = IMAGES
+		erb :images
+	end
+
+	get "/images/:index" do |index|
+		@image = IMAGES[index.to_i]
+		haml :"images/view", layout: true
+	end
+
+	get '/' do
+		"Hello World"
+	end
+
+	post '/' do
+		"Hello from POST"
+	end
+
+	put '/' do
+		"Hello via PUT"
+	end
+
+	put '/' do
+		"Hello via DELETE"
+	end
+
+	get '/hello/:name/?:last_name?' do |name, last|
+		"Hello #{name} #{last}"
+	end
+end
